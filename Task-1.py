fa5a4aea2db054ab0a35968c18ddbf55935cc21d
Pragya Mourya <69316226+Pragya26-code@users.noreply.github.com>
2025-06-12 11:37:00 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,15 @@
+#Perform Basic Mathematical Operations
+
+F= input('Enter the first number:')
+S= input('Enter the second number:')
+
+add = int(F) + int(S)
+sub = int(F) - int(S)
+mul = int(F) * int(S)
+div = int(F) / int(S)
+
+print('Addition:',add)
+print('Subtraction:',sub)
+print('Multiplication:',mul)
+print('Division:',div)
+
---
+++ Task-2.py
@@ -0,0 +1,9 @@
+# Create a Personalized Greeting
+
+f= input('Enter the First name:')
+l=input('Enter the Last name:')
+
+f= str(f)
+l= str(l)
+
+print("Hello " + f +  l + "!" + " welcome to Python program")
